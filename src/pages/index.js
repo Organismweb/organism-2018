2f5dbb2e848e903ba225034353c9c324eac06c59
Jonathan Selwall <jonathan@selwall.se>
2018-03-12 23:21:13 +0100

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,10 @@ import styled from 'styled-components'
 import * as THREE from 'three'
 import titleFontFile from '../fonts/cormorant-garamond-bold.json'
 import subTitleFontFile from '../fonts/barlow-regular.json'
+import textureImage from '../images/texture.png'
+import fragmentShader from 'webpack-glsl!../shaders/fragmentShader.glsl'
+
+import vertexShader from 'webpack-glsl!../shaders/vertexShader.glsl'
 
 class IndexPage extends Component {
   componentDidMount() {
@@ -22,71 +26,85 @@ class IndexPage extends Component {
 
     // Renderer
     const renderer = new THREE.WebGLRenderer({canvas, antialias: true})
-    renderer.setPixelRatio( window.devicePixelRatio )
+    renderer.setPixelRatio(window.devicePixelRatio)
     renderer.setSize(width, height)
 
     // Ambient light
-    const ambientLight = new THREE.AmbientLight( 0x404040, 0.5 ); 
+    const ambientLight = new THREE.AmbientLight(0x404040, 0.5)
     ambientLight.name = 'Ambient Light'
-    scene.add( ambientLight );
+    scene.add(ambientLight)
 
     // Directional light
     const pointerLight = new THREE.PointLight(0xffffff, 0.5)
     pointerLight.name = 'Pointer Light'
-    scene.add( pointerLight )
-  
+    scene.add(pointerLight)
+
     // Title font
     let fontLoader = new THREE.FontLoader()
     const titleFont = fontLoader.parse(titleFontFile)
-    
+
     const titleText = 'Organism'
-    const titleMaterial = new THREE.MeshBasicMaterial( {
-      color: new THREE.Color(0xFF001F),
+    const titleMaterial = new THREE.MeshBasicMaterial({
+      color: new THREE.Color(0xff001f),
     })
-    const titleShapes = titleFont.generateShapes( titleText, 42);
-    const titleGeo = new THREE.ShapeGeometry( titleShapes )
+    const titleShapes = titleFont.generateShapes(titleText, 42)
+    const titleGeo = new THREE.ShapeGeometry(titleShapes)
 
     titleGeo.computeBoundingBox()
     titleGeo.center()
 
-    const title = new THREE.Mesh( titleGeo, titleMaterial )
+    const title = new THREE.Mesh(titleGeo, titleMaterial)
     title.position.z = -400
 
-    scene.add( title )
-    
+    scene.add(title)
+
     // Subtitle font
     const subTitleFont = fontLoader.parse(subTitleFontFile)
     const subTitleText = 'DIGITAL INNOVATION'
-    const subTitleMaterial = new THREE.MeshBasicMaterial( {
-      color: new THREE.Color(0xFFFFFF),
+    const subTitleMaterial = new THREE.MeshBasicMaterial({
+      color: new THREE.Color(0xffffff),
     })
-    const subTitleShapes = subTitleFont.generateShapes( subTitleText, 10);
-    const subTitleGeo = new THREE.ShapeGeometry( subTitleShapes )
+    const subTitleShapes = subTitleFont.generateShapes(subTitleText, 10)
+    const subTitleGeo = new THREE.ShapeGeometry(subTitleShapes)
     subTitleGeo.computeBoundingBox()
     subTitleGeo.center()
-    const subTitle = new THREE.Mesh( subTitleGeo, subTitleMaterial )
+
+    const subTitle = new THREE.Mesh(subTitleGeo, subTitleMaterial)
     subTitle.position.z = -400
     subTitle.position.y = -44
-    scene.add( subTitle )
+    scene.add(subTitle)
 
-    // sphere
-    const sphereGeo = new THREE.SphereGeometry( 10, 200, 200 );
-    const sphereMaterial = new THREE.MeshPhongMaterial( {
-      color: 0xffffff,
-   
-    } );
-    const sphere = new THREE.Mesh( sphereGeo, sphereMaterial );
-    sphere.position.z = -40
-    sphere.position.x = -11
-    sphere.position.y = -3
-    scene.add( sphere );
+    // create a wireframe material
+    const material = new THREE.ShaderMaterial({
+      tExplosion: {
+        type: 't',
+      },
+      uniforms: {
+        time: {
+          // float initialized to 0
+          type: 'f',
+          value: 0.0,
+        },
+      },
+      vertexShader: vertexShader,
+      fragmentShader: fragmentShader,
+    })
 
+    // create a sphere and assign the material
+    const mesh = new THREE.Mesh(new THREE.IcosahedronGeometry(20, 4), material)
+    scene.add(mesh)
+    mesh.position.z = -80
+    mesh.position.x = -21
+    mesh.position.y = -3
+    scene.add(mesh)
 
     window.scene = scene
     window.THREE = THREE
 
+    const start = Date.now()
     const animate = () => {
       renderer.render(scene, camera)
+      material.uniforms['time'].value = 0.00025 * (Date.now() - start)
       requestAnimationFrame(animate)
     }
 
@@ -95,12 +113,10 @@ class IndexPage extends Component {
   render() {
     return (
       <React.Fragment>
-        <canvas ref={ canvas => this.canvas = canvas } />
+        <canvas ref={(canvas) => (this.canvas = canvas)} />
       </React.Fragment>
     )
   }
 }
 
 export default IndexPage
-
-
